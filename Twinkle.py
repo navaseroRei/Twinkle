a0db4882bd28485401a461fab46ea909f2c24a2b
Reister <rei.navasero@gmail.com>
2024-01-16 14:02:07 +0800

    first commit

---
+++ Twinkle.py
@@ -0,0 +1,6 @@
+first = "Twinkle, Twinkle, little star,"
+second = "How I wonder what you are!"
+third = "Up above the world so high,"
+fourth = "Like a diamond in the sky."
+
+print(first, "\n", "\t", second,"\n","\t","\t",third,"\n","\t","\t",fourth,"\n", first,"\n", "\t", second,)
